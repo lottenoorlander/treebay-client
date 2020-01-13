abc4e164ec8a8fbc130e45a978cecbf3a7b1ea1d
lottenoorlander <lottenoorlander@gmail.com>
2020-01-13 12:10:28 +0100

    stored login into local storage so when seller comes back from onboarding stripe they are still logged in

--- src/store/auth/reducer.js
+++ src/store/auth/reducer.js
@@ -1,15 +1,23 @@
+const jwt = sessionStorage.getItem("jwt");
+const user = sessionStorage.getItem("user");
+const isSeller = sessionStorage.getItem("isSeller");
+
 const initialState = {
-  jwt: null,
-  user: null,
-  isSeller: null
+  jwt: jwt ? jwt : null,
+  user: user ? user : null,
+  isSeller: isSeller ? isSeller : null
 };
 
 export default function authReducer(state = initialState, action) {
   switch (action.type) {
     case "USER_LOGGED_IN": {
+      sessionStorage.setItem("jwt", action.payload.jwt);
+      sessionStorage.setItem("user", action.payload.user);
+      sessionStorage.setItem("isSeller", action.payload.isSeller);
       return action.payload;
     }
     case "USER_LOGGED_OUT": {
+      sessionStorage.clear();
       return initialState;
     }
     case "USER_REQUESTED_LINK": {
